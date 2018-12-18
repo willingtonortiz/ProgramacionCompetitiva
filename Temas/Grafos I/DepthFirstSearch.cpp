46d215b731ed56a9401ad7456c15c6ee89de1c97
Willington Ortiz <34280023+willingtonortiz@users.noreply.github.com>
2018-12-18 10:16:57 -0500

    Create DepthFirstSearch.cpp

---
+++ Temas/Grafos I/DepthFirstSearch.cpp
@@ -0,0 +1,14 @@
+const int MAXG = 1000000;
+vector<int> graph[MAXG + 1];
+bool vis[MAXG + 1];
+
+void dfs(int cur){
+	if(vis[cur]) return;
+	vis[cur] = true;
+
+	int size = graph[cur].size();
+	for(int i = 0; i < size; ++i){
+		int v = graph[cur][i];
+		dfs(v);
+	}
+}
